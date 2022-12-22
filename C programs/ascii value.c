8ad82d25917742c16e58c02fbf9be56b77fac78f
Shrinath Asati <120880974+shrinathasati@users.noreply.github.com>
2022-12-22 18:07:15 +0530

    Add files via upload

---
+++ C programs/ascii value.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+	char ach;
+	printf("enter");
+	scanf("%c",&ach);
+	printf("%d",ach);
+
+}
---
+++ C programs/whileabcd.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+	int a;
+	printf("enter the value of a\n");
+	scanf("%d",&a);
+	while(a<=90)
+	{
+		printf("%c\n",a);
+		a++;
+		
+	}
+}
